df28a4d446ebed2ebff19e9e3e6493d0f1bf6e79
Suleman <sulemanpatras2@gmail.com>
2024-03-12 23:00:48 +0500

    Initial commit

---
+++ simple_WordPress_plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package Events
+ * @version 1.7.2
+ */
+/*
+Plugin Name: Events
+Description: This plugin will define a custom post type so you can manage events.
+Version: 1.7.2
+*/
+
+
+
+
+    
+
+
